1ace6a197965ccf9d102316102c115fe9fbd8e10
prd <prd@touk.pl>
2016-04-08 23:53:40 +0200

    4 finished

---
+++ euler4.py
@@ -0,0 +1,30 @@
+#https://projecteuler.net/problem=4
+
+def is_palindrome(n: int) -> bool:
+    string = str(n)
+    length = len(string)
+    for i in range(0, int(length / 2)):
+        if(string[i] != string[length - i - 1]):
+            return False
+    return True
+
+def biggest_palindrome_for_2_numbers(number_digits_for_each: int) -> (int):
+    assert number_digits_for_each > 0
+    maxnumber = 0
+    maxpalindrome = 0;
+    for i in range(0, number_digits_for_each):
+        maxnumber += (10 ** i) * 9
+    number1 = maxnumber
+    print (number1)
+    while(len(str(number1)) == number_digits_for_each):
+        number2 = maxnumber
+        while(len(str(number2)) == number_digits_for_each):
+            if is_palindrome(number1 * number2) and number1 * number2 > maxpalindrome:
+                maxpalindrome = number1 * number2
+            number2 -= 1
+        number1 -= 1
+    return maxpalindrome
+
+
+numbers = biggest_palindrome_for_2_numbers(3)
+print(numbers)
--- euler_tests.py
+++ euler_tests.py
@@ -2,7 +2,7 @@
 import unittest
 from euler1 import sum_3_5_multipliers_below
 from euler2 import sum_even_fibonacci_below
-from euler3 import is_prime, largest_prime_factor
+from euler4 import is_palindrome
 
 class Tests(unittest.TestCase):
     def test_euler1(self):
@@ -17,15 +17,13 @@ class Tests(unittest.TestCase):
         self.assertEqual(sum_even_fibonacci_below(4000000), 4613732)
 
 
-    def test_euler3(self):
-        self.assertEqual(is_prime(1), False)
-        self.assertEqual(is_prime(4), False)
-        self.assertEqual(is_prime(9), False)
-        self.assertEqual(is_prime(15), False)
-        self.assertEqual(is_prime(3), True)
-        self.assertEqual(is_prime(7), True)
-        self.assertEqual(is_prime(13), True)
-        self.assertEqual(largest_prime_factor(10), 5)
-        self.assertEqual(largest_prime_factor(9), 3)
+    def test_euler4(self):
+        self.assertEqual(is_palindrome(90), False)
+        self.assertEqual(is_palindrome(901), False)
+        self.assertEqual(is_palindrome(909109), False)
+        self.assertEqual(is_palindrome(909909), True)
+        self.assertEqual(is_palindrome(99), True)
+
+
 if __name__ == '__main__':
     unittest.main()
